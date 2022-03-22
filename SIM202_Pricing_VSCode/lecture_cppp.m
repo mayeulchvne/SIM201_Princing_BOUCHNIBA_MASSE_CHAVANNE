c132e6b28b41c60dd2d0d3d02fc9114e05bb761a
Mayeul Chavanne <mayeul.chavanne@gmail.com>
2022-03-22 10:19:45 +0100

    fichier de lecture en matlab

--- SIM202_Pricing_VSCode/lecture_cppp.m
+++ SIM202_Pricing_VSCode/lecture_cppp.m
@@ -1,39 +1,39 @@
+%% Fonction de lecture du fichier texte edité par le script main.cpp
 function [video,Nbpt,Nbtri,Numtri,Coorneu]=lecture_cppp(nomfile)
 fid=fopen(nomfile,'r');
 if fid <=0,
    msg=['Le fichier de maillage : ' nomfile ' n''a pas ete trouve'];
    error(msg);
 end
-Nbpt = str2num(fgetl(fid));
+Nbpt = str2int(fgetl(fid));
 Coorneu = zeros(Nbpt,2);
 
-tmp= str2num(fgetl(fid));
+tmp= str2double(fgetl(fid));
 for i=1:Nbpt
     Coorneu(i,:) = tmp(end-1:end);
 end
 
 
-Nbtri = str2num(fgetl(fid));
+Nbtri = str2int(fgetl(fid));
 Numtri = zeros(Nbtri,3);
 
-tmp= str2num(fgetl(fid));
+tmp= str2int(fgetl(fid));
 for i=1:Nbtri
     Numtri(i,:) = tmp(end-2:end);
 end
 
 
-temps=str2num(fgetl(fid));
+temps=str2int(fgetl(fid));
 video=zeros(Nbpt,temps);
 valeurs=zeros(Nbpt,1);
-tmp=str2num(fgetl(fid));
-fprintf('%i',tmp);
+tmp=str2double(fgetl(fid));
+
 for j=1:temps
     for i=1:Nbvaleurs
         valeurs(i)=tmp(end);
-        tmp=str2num(fgetl(fid));
+        tmp=str2double(fgetl(fid));
     end
-    disp(valeurs);
-    disp(video);
+   
     video(:,j)=valeurs(:);
 end
 
